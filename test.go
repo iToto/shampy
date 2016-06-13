809f250417fc2fa8ee293992fc119a1d0fde91e9
Anna <agyergyai@gmail.com>
2016-06-13 16:35:00 -0700

    starting to play with API endpoints

--- test.go
+++ test.go
@@ -1,7 +1,31 @@
 package main
 
-import "fmt"
+import (
+    "fmt"
+    "log"
+    "net/http"
+
+    "github.com/gorilla/mux"
+)
 
 func main() {
-    fmt.Printf("shampy is coming!!!\n")
+    router := mux.NewRouter().StrictSlash(true)
+    router.HandleFunc("/", Index)
+    router.HandleFunc("/shampoo", ShampooIndex)
+    router.HandleFunc("/shampoo/{id}", ShampooShow)
+    log.Fatal(http.ListenAndServe(":8080", router))
+}
+
+func Index(w http.ResponseWriter, r *http.Request) {
+    fmt.Fprintln(w, "Hello there and welcome to Shampy!")
+}
+
+func ShampooIndex(w http.ResponseWriter, r *http.Request) {
+    fmt.Fprintln(w, "ShampooIndex")
+}
+
+func ShampooShow(w http.ResponseWriter, r *http.Request) {
+    vars := mux.Vars(r)
+    shampooId := vars["id"]
+	fmt.Fprintln(w, "Shampoo show:", shampooId)
 }
