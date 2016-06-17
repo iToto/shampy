f0e7a6af1dc141f5b3f54b5040a2820b21023935
Anna <agyergyai@gmail.com>
2016-06-16 22:38:16 -0700

    playing around with map

--- test.go
+++ test.go
@@ -4,15 +4,24 @@ import (
     "fmt"
     "log"
     "net/http"
+    "encoding/json"
+    "strconv"
 
     "github.com/gorilla/mux"
 )
+
+var shampoos = map[int]Shampoo{1: {"someBrand"}}
+ 
+type Shampoo struct {
+    Brand string
+}
 
 func main() {
     router := mux.NewRouter().StrictSlash(true)
     router.HandleFunc("/", Index)
     router.HandleFunc("/shampoo", ShampooIndex)
     router.HandleFunc("/shampoo/{id}", GetShampoo).Methods("GET")
+    router.HandleFunc("/shampoo/{id}", PostShampoo).Methods("POST")
     log.Fatal(http.ListenAndServe(":8080", router))
 }
 
@@ -21,11 +30,18 @@ func Index(w http.ResponseWriter, r *http.Request) {
 }
 
 func ShampooIndex(w http.ResponseWriter, r *http.Request) {
-    fmt.Fprintln(w, "ShampooIndex")
+	w.Header().Set("Content-Type", "application/json")
+    j, _ := json.Marshal(shampoos)
+    w.Write(j)
 }
 
 func GetShampoo(w http.ResponseWriter, r *http.Request) {
     vars := mux.Vars(r)
-    shampooId := vars["id"]
-	fmt.Fprintln(w, "Shampoo show:", shampooId)
+    shampooId, _ := strconv.ParseInt(vars["id"], 10, 0)
+
+	fmt.Fprintln(w, "Shampoo show:", shampoos[int(shampooId)])
+}
+
+func PostShampoo(w http.ResponseWriter, r *http.Request) {
+
 }
